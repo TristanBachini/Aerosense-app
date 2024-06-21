08520adb5bc07944aff4b08832145535a2c6c2ad
Tristan Paul Bachini <bachinitristanpaul@gmail.com>
2024-06-21 15:38:26 -0700

    Made progress with login api

--- myenv/Assure_App/main/views.py
+++ myenv/Assure_App/main/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+import requests
 
 def home(request):
-
-    return render(request,"main/homepage.html")
+    
+    if(request.method == 'POST'):
+        print("allo")
+    return render(request,"main/login.html")
