33b57e77666b2df61fc43bef013f00ebb1c19c99
Tristan Paul Bachini <bachinitristanpaul@gmail.com>
2024-07-02 17:06:49 -0700

    made progress with login, began setup for mysql integration

--- myenv/Assure_App/main/views.py
+++ myenv/Assure_App/main/views.py
@@ -1,8 +1,33 @@
 from django.shortcuts import render
 import requests
 
+token = ''
+ip_address = ''
+
 def home(request):
     
     if(request.method == 'POST'):
-        print("allo")
+
+        url = "http://192.168.8.100/api/login"
+
+        payload = "{\r\n    \"username\":\"admin\",\r\n    \"password\":\"admin\"\r\n}"
+        headers = {
+        'Content-Type': 'application/json;charset=UTF-8'
+        }
+
+        response = requests.request("POST", url, headers=headers, data=payload)
+
+        data = response.json()
+        
+        #
+        token = data.get('data', {}).get('token')
+
+
+
     return render(request,"main/login.html")
+
+def set_token(request, key):
+    
+    #Set global token 
+    global token 
+    token = key
